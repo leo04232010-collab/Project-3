0b662bd3d46c785bceb15873108b83b2c271c91f
LeoDeVinki <leo04232010@gmail.com>
2026-07-08 13:11:01 -0700

    tenk

---
+++ Assets/[Scripts]/test.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    public string name;
+    // Start is called before the first frame update
+    bool answer1 = true && true;
+
+    void Start()
+    {
+        Debug.Log(answer1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+}
